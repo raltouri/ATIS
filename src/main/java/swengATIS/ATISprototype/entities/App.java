ee2f5008736cb7db9b9357fcb55cc51669ca5abe
raltouri <rashad.altouri@gmail.com>
2024-03-14 12:15:51 +0200

    Initial commit

---
+++ src/main/java/swengATIS/ATISprototype/entities/App.java
@@ -0,0 +1,13 @@
+package swengATIS.ATISprototype.entities;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
